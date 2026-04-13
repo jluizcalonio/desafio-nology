e2571d60d699958707490090ff6bc4eaa19f53c7
jluizcalonio <maiacalonio@gmail.com>
2026-04-13 19:43:28 -0300

    Commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import mysql.connector
 import os
